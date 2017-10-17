7b65257b10e5541da760cf314e1c4fa79eb00d34
evaccaro <erv230@nyu.edu>
2017-10-17 18:04:02 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+
+attr_accessor :artist, :name
+
+def initialize(name)
+  @name = name
+end
+
+
+def artist_name
+  if self.artist
+  self.artist.name
+else
+  self.artist = nil
+end
+end
+
+end
